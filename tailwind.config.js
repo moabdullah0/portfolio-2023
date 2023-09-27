216ec4c888c4247145f95e8ab8d7739f80eb6ddd
mohamad <mohamad.alibrahim1998@gmail.com>
2023-09-27 21:10:12 +0300

    fourth

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
-  
   theme: {
     extend: {},
+  },
+  animation: {
+    text: 'text 5s ease infinite',    
+  },
+  keyframes: {
+    text: {
+      '0%, 100%': {
+        'background-size': '200% 200%',
+        'background-position': 'left center'
+      },
+      '50%': {
+        'background-size': '200% 200%', 
+        'background-position': 'right center'
+      }
+    }
   },
   plugins: [],
 }
