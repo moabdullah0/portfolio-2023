7a52e5dfddf737a2ffba6516b64b504e8e3dfdda
mohamad <mohamad.alibrahim1998@gmail.com>
2023-10-02 08:55:14 +0300

    reveal

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,7 @@
 let list = document.getElementById('list');
 let exit = document.getElementById('exit');
 let menu = document.querySelector('.menu');
+let reveals =document.querySelectorAll('.reveal')
 
 list.addEventListener('click', (e) => {
   menu.classList.remove('hiddenlist');
@@ -17,3 +18,32 @@ exit.addEventListener('click', (e) => {
   exit.style.visibility = "hidden";
   list.style.display = "block";
 });
+
+//reveal
+
+
+
+
+// Function to check if an element is in the viewport
+function isElementInViewport(el) {
+  const windowHeight = window.innerHeight;
+  const revealTop = el.getBoundingClientRect().top;
+  const revealPoint = 150;
+
+  return revealTop < windowHeight - revealPoint;
+}
+
+// Function to handle the scroll event
+function handleScroll() {
+  for (let i = 0; i < reveals.length; i++) {
+    if (isElementInViewport(reveals[i])) {
+      reveals[i].classList.add('active');
+    }
+  }
+}
+
+// Add an event listener for the scroll event
+window.addEventListener('scroll', handleScroll);
+
+
+handleScroll();
